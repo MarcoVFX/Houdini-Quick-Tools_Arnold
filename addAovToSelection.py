d499c543c1329de672d20d4d4b75499e9875f8d5
MarcoVFX <136552079+MarcoVFX@users.noreply.github.com>
2024-02-25 01:26:54 -0800

    Update addAovToSelection.py

--- addAovToSelection.py
+++ addAovToSelection.py
@@ -7,29 +7,38 @@ iteration = 0;
 nodeCollection = []
 
 for node in nodes:
-    #create AOV nodes
-    parent = node.parent()
-    parentPath = parent.path()
-    name = node.name()
-    aovwrite = parent.createNode("arnold::aov_write_rgb",name+"__",force_valid_node_name=True)
+    type = node.type().name()
     
-    #set parm and input
-    aovwrite.parm("aov_name").set(name)
-    aovwrite.setInput(1,node,0)
-    aovwrite.setSelected(True)
-    
-    #add node into collection for layout
-    nodeCollection.append(node)
-    nodeCollection.append(aovwrite)
-      
-    #set input
-    if (iteration>0):
-        lastNode.setInput(0,aovwrite,0)
-            
-    #create last node
-    lastNode = aovwrite
-    iteration += 1;
+    if (type == "arnold::image"):
+        #create AOV nodes
+        parent = node.parent()
+        parentPath = parent.path()
+        name = node.name()
+        aovwrite = parent.createNode("arnold::aov_write_rgb",name+"__",force_valid_node_name=True)
+        
+        #set parm and input
+        aovwrite.parm("aov_name").set(name)
+        aovwrite.setInput(1,node,0)
+        aovwrite.setSelected(True)
+        
+        #add node into collection for layout
+        nodeCollection.append(node)
+        nodeCollection.append(aovwrite)
+          
+        #set input
+        if (iteration>0):
+            lastNode.setInput(0,aovwrite,0)
+                
+        #create last node
+        lastNode = aovwrite
+        iteration += 1;
 
-#layout    
+    if (type != "arnold::image"):
+        print ("You should NOT select any non-image nodes")
+        raise hou.NodeError("You should NOT select any non-image nodes!")
+        
+        
+#layout  
+
 parent.layoutChildren(items=(nodeCollection))
 
